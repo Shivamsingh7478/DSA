2156244142ba2091d4ee963ddf11b100b78b353e
Shivam Singh <shivamsingh2681@gmail.com>
2025-08-23 09:05:36 +0530

    Time: 4 ms (52.16%), Space: 11.9 MB (30.03%) - LeetHub

--- 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.cpp
+++ 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.cpp
@@ -1,19 +1,70 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size() == 0)
+        // if(s.size() == 0)
+        // {
+        //     return true;
+
+        // }
+
+        // int fnd = s.find("abc");
+
+        // if(fnd != string::npos)
+        // {
+        //     string left = s.substr(0, fnd);
+        //     string right = s.substr(fnd+3, s.size());
+        //     return isValid(left+right);
+        // }
+
+        // return false;
+
+
+        if(s[0] != 'a')
         {
-            return true;
+            return false;
 
         }
-
-        int fnd = s.find("abc");
-
-        if(fnd != string::npos)
+        stack<char> st;
+        for(char ch:s)
         {
-            string left = s.substr(0, fnd);
-            string right = s.substr(fnd+3, s.size());
-            return isValid(left+right);
+            if(ch == 'a')
+            {
+                st.push('a');
+            }
+           else if(ch == 'b')
+           {
+            if(!st.empty() && st.top() == 'a')
+            {
+                st.push('b');
+            }
+            else
+            {
+                return false;
+            }
+           }
+
+           else{
+            if(!st.empty() && st.top() == 'b')
+            {
+                st.pop();
+                if(!st.empty() && st.top() == 'a')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+           }
+        }
+
+        if(st.empty())
+        {
+            return true;
         }
 
         return false;
