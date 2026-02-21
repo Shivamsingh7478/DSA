5669e940cb1439407e23f5313a88dd738a87cd1a
Shivam Singh <shivamsingh2681@gmail.com>
2026-02-22 01:17:00 +0530

    Time: 0 ms (100%), Space: 47.2 MB (5.31%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,20 +14,45 @@
  * }
  */
 class Solution {
-    public void inOrder(TreeNode root, List<Integer> vector)
+    // public void inOrder(TreeNode root, List<Integer> vector)
+    // {
+    //     if(root== null)
+    //     {
+    //         return;
+    //     }
+
+    //     inOrder(root.left, vector);
+    //     vector.add(root.val);
+    //     inOrder(root.right, vector);
+    // }
+
+    int count =0;
+    TreeNode ans = null;
+    public void solve(TreeNode root, int k)
     {
-        if(root== null)
+        if(root == null)
         {
             return;
         }
 
-        inOrder(root.left, vector);
-        vector.add(root.val);
-        inOrder(root.right, vector);
+        solve(root.left, k);
+        count++;
+        if(count == k)
+        {
+            ans = root;
+        }
+
+        solve(root.right, k);
     }
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> arr = new ArrayList<>();
-        inOrder(root, arr);
-        return arr.get(k-1);
+        // List<Integer> arr = new ArrayList<>();
+        // inOrder(root, arr);
+        // return arr.get(k-1);
+        
+        solve(root, k);
+        return ans.val;
+
+
+
     }
 }
